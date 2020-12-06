1e8519e0c12bcd4f8ee4acb9c67a53c38642be3e
ikramasifkhan <iakriubat2016@gmail.com>
2020-12-07 00:45:18 +0600

    invoice_details_table added and configured

---
+++ database/migrations/2020_12_06_183127_create_invoice_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_details', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->foreignId('invoice_id')->constrained('invoices')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->integer('selling_qty');
+            $table->decimal('unit_price');
+            $table->decimal('total_price');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_details');
+    }
+}

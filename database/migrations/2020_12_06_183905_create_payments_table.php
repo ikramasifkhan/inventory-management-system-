2b48c31d22a516f0f2365629325d82cbcb4cea55
ikramasifkhan <iakriubat2016@gmail.com>
2020-12-07 00:51:38 +0600

    payment table added and configured

---
+++ database/migrations/2020_12_06_183905_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id')->constrained('invoices')->onDelete('cascade');
+            $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
+            $table->string('paid_status', 64)->nullable();
+            $table->decimal('paid_amount')->nullable();
+            $table->decimal('due_amount')->nullable();
+            $table->decimal('total_amount')->nullable();
+            $table->decimal('discount_amount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
